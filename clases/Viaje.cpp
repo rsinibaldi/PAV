9ca322ef5cf73b9ddc425532a1cb9972008bf9a3
rsinibaldi <ro.sinibaldi@gmail.com>
2020-03-18 19:22:50 -0300

    viaje.cpp

--- clases/Viaje.cpp
+++ clases/Viaje.cpp
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "../clases/Viaje.h"
-#include "../clases/Usuario.h"
-
-using namespace std;
-
-Viaje::Viaje() {}
-Viaje::Viaje(DtFecha fecha, int duracion, int distancia, Usuario* usuario){
-    this-> fechaIngreso=fecha;
-    this -> duracion=duracion;
-    this -> distancia=distancia;
-
-}
-=======
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,4 +12,3 @@ Viaje::Viaje(DtFecha fecha, int duracion, int distancia){
     this -> duracion=duracion;
     this -> distancia=distancia;
 }
->>>>>>> abe0b81931deaeb0ac6f427a00403a2130f20a91
