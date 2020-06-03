40e591c59a3761701c94263413aa31676fb22925
rsinibaldi <ro.sinibaldi@gmail.com>
2020-06-03 00:18:41 -0300

    comenzando RS

---
+++ clases/Empleado.cpp
@@ -0,0 +1,25 @@
+#include "Empleado.h"
+
+//Constructores
+Empleado::Empleado() {}
+Empleado::Empleado(string idEmpleado, string nombre) {
+    this->idEmpleado = idEmpleado;
+    this->nombre = nombre;
+}
+
+//Getters & Setters
+string Empleado::getIdEmpleado() {
+    return this->idEmpleado;
+}
+void Empleado::setidEmpleado(string idEmpleado) {
+    this->idEmpleado = idEmpleado;
+}
+string Empleado::getNombreEmpleado() {
+    return  this->nombre;
+}
+void Empleado::setNombreEmpleado(string nombre) {
+    this->nombre = nombre;
+}
+
+//Destructores
+Empleado::~Empleado() {}
