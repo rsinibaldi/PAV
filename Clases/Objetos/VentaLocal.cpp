8634410bccc42910a6d60852dfb93e9abd0a7ac8
mike <yokadance@gmail.com>
2020-06-10 20:46:23 -0300

    ventaLocal Mike

--- Clases/Objetos/VentaLocal.cpp
+++ Clases/Objetos/VentaLocal.cpp
@@ -1,5 +1,40 @@
-//
-// Created by mike on 6/3/20.
-//
-
 #include "VentaLocal.h"
+#include <string.h>
+#include <iostream>
+#include "Venta.h"
+#include "Producto.h"
+#include "VentaProducto.h"
+#include "../Manejadores/ManejadorVenta.h"
+
+
+using namespace std;
+
+VentaLocal::VentaLocal(){}
+
+bool VentaLocal::tieneProducto(string codigoProducto){
+    int i=0;
+    bool tiene= false;
+    ManejadorVenta* mv= ManejadorVenta::getInstancia();
+    VentaProducto* ventaProd ;
+    Venta* venta;
+    string codV= ventaProd->getCodigoProducto();
+    while(!tiene && i< 
+
+
+}
+
+void VentaLocal::agregarProducto(DtProductoCantidad pc){
+
+}
+
+void VentaLocal::incrementar(DtProductoCantidad pc){
+
+}
+
+map<DtProducto*, int> VentaLocal::listarProductos(){
+
+}
+
+list<DtProductoFactura*> VentaLocal::getDtProductoFactura(){
+
+}
--- Clases/Objetos/VentaLocal.h
+++ Clases/Objetos/VentaLocal.h
@@ -2,9 +2,23 @@
 #define PAV_VENTALOCAL_H
 
 
-class VentaLocal {
+#include "Venta.h"
+#include "Clases/DataTypes/DtProducto.h"
+#include "Clases/DataTypes/DtProductoFactura.h"
+#include "Clases/DataTypes/DtProductoCantidad.h"
+#include <map>
+#include <list>
 
+class VentaLocal: public Venta {
+public:
+    VentaLocal();
+    bool tieneProducto(string codigoProducto);
+    void agregarProducto(DtProductoCantidad pc);
+    void incrementar(DtProductoCantidad pc);
+    map<DtProducto*, int> listarProductos();
+    list<DtProductoFactura*> getDtProductoFactura();
+    string getNombreMozo(string idMozo);
+    float getSubtotalVenta();
 };
-
 
 #endif //PAV_VENTALOCAL_H
